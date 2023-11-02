85127085e0a6c96176248f8630019ae2c8a688e1
alencshaji <alencshaji@gmail.com>
2023-11-02 16:00:05 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-      
+      <div>
+      <section   className='fixed-bottom bg-light' style={{padding:'1rem'}}>
+			<div className="copy-right-sec"><i className="fa-solid fa-copyright"></i>  
+			Contact App @ 2023
+			</div>
+
+		</section>
+    </div>
     </div>
   )
 }
